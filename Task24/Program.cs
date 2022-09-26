dc46cd69ecdc5affb08bcbecb90fbacba2007dc2
Svetlana Lap <laptevas80@gmail.com>
2022-09-26 22:57:47 +0300

    Task4

---
+++ Task24/Program.cs
@@ -0,0 +1,15 @@
+﻿// void Zadacha24()
+// {
+// // Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+int number = 5;
+int sum = 0;
+
+for (int i = 1; i <= number; i++)
+{
+    // Console.Write($"{i}");
+    sum += i;
+}
+    Console.Write($"{sum}");
+
+Zadacha24();
+
