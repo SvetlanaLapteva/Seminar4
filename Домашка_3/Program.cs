a7096ae87ed3ea98fe43469c648e1e7b560f71db
Svetlana Lap <laptevas80@gmail.com>
2022-09-26 22:56:47 +0300

    Домашка_3

---
+++ Домашка_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача  19
+
+// int number = 13431;
+// int current_number = number;
+// int result = 0;
+// while (number > 0 )
+// {
+//     int digit = number % 10;
+//     result = result * 10 + digit;
+//     current_number /= 10;
+//     Console.WriteLine(result);
+// }
+// if (number == result)
+// {
+//     Console.WriteLine($"число{number} палиндром");
+// }
+// else 
+// {
+//     Console.WriteLine($"число{number} не палиндром");
+// }
+// Задача 21
+// int a_x = 1;
+// int a_y = 1;
+// int a_z - 1;
+// int b_x = 0;
+// int b_y - 0;
+// int b_z = 0;
+
+// int result = Math.Sqrt(Math.Pow(a_x-b_x, 2))+Math.Pow(a_y - b_x, 2)
+
+// Задача 23
+int number = 15;
+for (int i = 1; i <= number; i++)
+{
+    Console.WretiLine(${i} * {i} * {i})
+}
