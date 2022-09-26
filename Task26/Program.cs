280212b1ded03681b78df687c21c1651b7b19731
Svetlana Lap <laptevas80@gmail.com>
2022-09-26 23:07:11 +0300

    Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,25 @@
+﻿// void Zadacha26();
+// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+// {
+//     int number = 1234322;
+//     int current = number;
+//     int count = 0;
+//     while (current > 0)
+//     {
+//         current = current /10;
+//         count = count + 1;
+//     }
+//     Console.WriteLine($"Количество цифр в числе {number} = {count}");
+// }
+
+// Zadacha26();
+
+void Task26()
+            {
+                Console.WriteLine("\n \t Task 24");
+                Console.Write("Enter number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"{num} -> {Math.Ceiling(Math.Log10(num))}");
+            }
+            Task26();
